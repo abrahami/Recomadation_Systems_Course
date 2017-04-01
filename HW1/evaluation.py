e87bf0a545b1c55239f742bad66cd1f5ef1c344b
abrahami <abraham.israeli@intel.com>
2017-04-01 20:59:02 +0300

    Added and evaluation class, and added few lines in the main file to show how to run it

---
+++ HW1/evaluation.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+__author__ = 'avrahami'
+
+
+class Evaluation(object):
+    '''
+    a class to evaluate results, calculates the rmse and mse of any given two vectors
+
+    Parameters
+    ----------
+
+    Attributes
+    -----------
+    rmse: float
+        root mean square error
+    mse: float
+        mean absolute error
+    '''
+
+    def __init__(self):
+        self.rmse = None
+        self.mae = None
+
+    def evaluate(self, true_ranks, predicted_ranks):
+        '''
+        the actual evaluation process
+
+        Parameters
+        ----------
+        true_ranks: list or numpy array
+            the real rank values of each user with each item. Correctly it is given as a vector (1-dim), if needed to be
+            changed in the future, we can convert is to a matrix
+        predicted_ranks: list or numpy array
+            the predicted rank values of each user with each item. Correctly it is given as a vector (1-dim), if needed
+            to be changed in the future, we can convert is to a matrix
+
+        Returns
+        -------
+        all is stored in the class parameters (nothing is currently returned by the function
+        '''
+
+        if len(true_ranks) != len(predicted_ranks):
+            print "length of the two vectors must be the same, please check again"
+        # convert both list to numpy array (if they are not so already) - it will help us with all calculations
+        true_ranks = np.array(true_ranks)
+        predicted_ranks = np.array(predicted_ranks)
+
+        N = len(true_ranks)
+        # calculating all the importatn measures we care about
+        avg_power_error = np.sum(np.power((true_ranks-predicted_ranks), 2))*1.0/N
+        avg_abs_error = np.sum(np.abs(true_ranks - predicted_ranks))*1.0/N
+        self.rmse = np.sqrt(avg_power_error)
+        self.mae = avg_abs_error
--- HW1/main.py
+++ HW1/main.py
@@ -1,4 +1,5 @@
 import loader
+import evaluation
 
 # example of loading one specific category (here it is 'pets')
 my_path = "C:\\Users\\abrahami\\Documents\\Private\\Uni\\BGU\\Reco.Systems - Bracha\\HW1\\Yelp"
@@ -11,7 +12,7 @@ print "Loading has just finished, shape of the train dataset is {}," \
       " shape of the test dataset is {}".format(train.shape, test.shape)
 
 
-# of loading all categories
+# example of loading all categories
 my_path = "C:\\Users\\abrahami\\Documents\\Private\\Uni\\BGU\\Reco.Systems - Bracha\\HW1\\Yelp"
 loader_obj = loader.Loader(path=my_path)
 data = loader_obj.load(d_train_all_categories=True)
@@ -20,3 +21,10 @@ test = data['test_data']
 
 print "Loading has just finished, shape of the train dataset is {}," \
       " shape of the test dataset is {}".format(train.shape, test.shape)
+
+# example of evaluation process
+true_vec = [1, 2, 3, 4, 5]
+pred_vec = [3, 3, 3, 3, 3]
+eval_obj = evaluation.Evaluation()
+eval_obj.evaluate(true_ranks=true_vec, predicted_ranks=pred_vec)
+print "Evaluation has just ended, the RMSE is {}, the MAE is {}".format(eval_obj.rmse, eval_obj.mae)
