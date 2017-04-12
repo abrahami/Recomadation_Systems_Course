36b96c4ea650cff7e276defe37d58589c65c0cb8
abrahami <abraham.israeli@intel.com>
2017-04-12 08:39:18 +0300

    Fixing a bug when prediction the rank to new instances

--- HW1/main.py
+++ HW1/main.py
@@ -15,7 +15,7 @@ test = data['test_data']
 print "Loading has just finished, shape of the train dataset is {}," \
       " shape of the test dataset is {}".format(train.shape, test.shape)
 
-'''
+
 # example of loading all categories
 my_path = "C:\\Users\\abrahami\\Documents\\Private\\Uni\\BGU\\Reco.Systems - Bracha\\HW1\\Yelp"
 loader_obj = loader.Loader(path=my_path)
@@ -33,6 +33,7 @@ pred_vec = [3, 3, 3, 3, 3]
 eval_obj = evaluation.Evaluation()
 eval_obj.evaluate(true_ranks=true_vec, predicted_ranks=pred_vec)
 print "Evaluation has just ended, the RMSE is {}, the MAE is {}".format(eval_obj.rmse, eval_obj.mae)
+'''
 
 # example of running a recommendation engine process
 svd_obj = recommender_system.RecommenderSystem()
--- HW1/recommender_system.py
+++ HW1/recommender_system.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import evaluation
+from datetime import datetime
 
 __author__ = 'avrahami'
 
@@ -24,16 +25,20 @@ class RecommenderSystem(object):
     def __init__(self):
         self.rmse = np.inf
         self.final_model = None
+        self._average = None
         self._users = None
         self._items = None
         self._full_pred_matrix = None
+        self._b_u = None
+        self._b_i = None
 
     def _provide_pred(self, single_row, train_data):
         existing_user = single_row.UserID in self._users
         existing_item = single_row.ItemID in self._items
         # case we have the userID and the itemID in our training set and we can pull the prediction easily
         if existing_user and existing_item:
-            return self._full_pred_matrix.loc[single_row.UserID, single_row.ItemID]
+            dot_prod = self._full_pred_matrix.loc[single_row.UserID, single_row.ItemID]
+            return self._average + self._b_u.loc[single_row.UserID] + self._b_i.loc[single_row.ItemID] + dot_prod
         # case we only have the UserID in our training set (case of a new item to the system) - we'll return the average
         # of ranking for this user
         elif existing_user:
@@ -57,6 +62,7 @@ class RecommenderSystem(object):
         p_u = np.random.uniform(-0.1, 0.1, size=(len(self._users), latent_size))
         q_i = np.random.uniform(-0.1, 0.1, size=(len(self._items), latent_size))
         avg = np.average(train_data["Rank"])
+        self._average=avg
 
         p_u_df = pd.DataFrame(p_u, index=list(self._users), columns=range(0, latent_size))
         q_i_df = pd.DataFrame(q_i, index=list(self._items), columns=range(0, latent_size))
@@ -66,21 +72,25 @@ class RecommenderSystem(object):
         # building the evaluation object to be used inside the loop
         eval_obj = evaluation.Evaluation()
         for i in xrange(iterations):
+            start_time = datetime.now()
             for index, row in train_data.iterrows():
                 cur_user = row["UserID"]
                 cur_item = row["ItemID"]
                 cur_rank = row["Rank"]
-                dot_prod = sum(p_u_df.loc[cur_user]*q_i_df.loc[cur_item])
+                dot_prod = sum(p_u_df.loc[cur_user] * q_i_df.loc[cur_item])
                 cur_error = float(cur_rank-avg-b_u_df.loc[cur_user]-b_i_df.loc[cur_item]-dot_prod)
-                b_u_df.loc[cur_user] += lamda*(cur_error-gamma*b_u_df.loc[cur_user])
+                b_u_df.loc[cur_user] += lamda * (cur_error - gamma * b_u_df.loc[cur_user])
                 b_i_df.loc[cur_item] += lamda * (cur_error - gamma * b_i_df.loc[cur_item])
-                q_i_df.loc[cur_item] += lamda * (cur_error*p_u_df.loc[cur_user] - gamma * q_i_df.loc[cur_item])
-                p_u_df.loc[cur_user] += lamda * (cur_error*q_i_df.loc[cur_item] - gamma * p_u_df.loc[cur_user])
+                q_i_df.loc[cur_item] += lamda * (cur_error * p_u_df.loc[cur_user] - gamma * q_i_df.loc[cur_item])
+                p_u_df.loc[cur_user] += lamda * (cur_error * q_i_df.loc[cur_item] - gamma * p_u_df.loc[cur_user])
             # end of inner loop, now calculating the errors and decide if to go for another loop
             self._full_pred_matrix = p_u_df.dot(q_i_df.transpose())
+            self._b_i = b_i_df
+            self._b_u = b_u_df
             validation_pred = validation_data.apply(lambda x:  self._provide_pred(single_row=x, train_data=train_data), axis=1)
             eval_obj.evaluate(true_ranks=list(validation_data.Rank), predicted_ranks=list(validation_pred))
-            print "Loop number {}, the RMSE is {}".format(i, eval_obj.rmse)
+            duration = (datetime.now() - start_time).seconds
+            print "Loop number {}, the RMSE is {}, this loop took us {} minutes".format(i, eval_obj.rmse, duration/60.0)
             if eval_obj.rmse < self.rmse:
                 self.rmse = eval_obj.rmse
             else:
